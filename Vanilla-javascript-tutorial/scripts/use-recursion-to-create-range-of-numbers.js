bee46ae9f2be4810f749a7911e8942389439c881
Luis <luistroche@gmail.com>
2020-09-21 23:52:05 -0400

    Add code like content to parent tag w/ getElementbyId().innerHtml.

--- Vanilla-javascript-tutorial/scripts/use-recursion-to-create-range-of-numbers.js
+++ Vanilla-javascript-tutorial/scripts/use-recursion-to-create-range-of-numbers.js
@@ -5,6 +5,7 @@ document.getElementsByTagName("title")[0].innerHTML = "Use Recursion to Create a
 document.querySelector("h1").innerHTML = "Use Recursion to Create a Range of Numbers";
 // var elementoH1 = document.getElementsByTagName("h1#htitle");
 // console.log(elementoH1);
+/*
 function rangeOfNumbers(startNum, endNum) {
   if (startNum === endNum){
     return [startNum];
@@ -17,4 +18,25 @@ function rangeOfNumbers(startNum, endNum) {
 }
 console.log(rangeOfNumbers(1, 5));
 console.log(rangeOfNumbers(6, 9));
-console.log(rangeOfNumbers(4, 4));
+console.log(rangeOfNumbers(4, 4));*/
+
+
+var contenido =
+
+"function rangeOfNumbers(startNum, endNum) {  \n" + "</br> " +
+" if (startNum === endNum){ \n" + " </br> " +
+" return [startNum \n" + " </br> " +
+"}else{ \n" + " </br> " +
+"let numeros = rangeOfNumbers(startNum, endNum -1); \n" + " </br> " +
+"numeros.push(endNum); \n" + " </br> " +
+"return numeros; \n" + " </br> " +
+"} \n" + " </br> " +
+"} \n \n" + " </br> " +
+"console.log(rangeOfNumbers(1, 5)); \n" + " </br> " +
+"console.log(rangeOfNumbers(6, 9)); \n" + " </br> " +
+"console.log(rangeOfNumbers(4, 4));\n" + " </br> " ;
+
+// document.getElementById("codigo").innerText = contenido;
+// console.log(document.getElementById("codigo"));
+
+document.getElementById("codigo").innerHTML = '<article> '+ contenido +' </article';
