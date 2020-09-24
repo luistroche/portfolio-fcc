e18a650a0277d0b500d92595b57cea6b9648ef61
Luis <luistroche@gmail.com>
2020-09-24 18:58:45 -0400

    adding spelling directory html js

--- Vanilla-javascript-tutorial/scripts/spelling.js
+++ Vanilla-javascript-tutorial/scripts/spelling.js
@@ -28,14 +28,3 @@ console.log(contenido[0][3]);
 filtrado.push(contenido[0][3]);
 console.log(filtrado + `<--`);
 console.log(contenido);
-for( var i = 0; i < contenido.length; i++){
-    console.log(contenido[0][i]);
-    if (contenido1[0][i] == ["a", "b", "c", "m"]) {
-        console.log(`-->${contenido1[0][i]}<--`);
-
-    }else{
-
-        console.log(contenido[0][i]);
-
-    }
-}
