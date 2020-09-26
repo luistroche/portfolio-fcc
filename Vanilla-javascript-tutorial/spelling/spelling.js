246d662766856db1a1d4501723c655d9110d12ad
Luis <luistroche@gmail.com>
2020-09-26 00:29:48 -0400

    spelling.js starting basic formating

--- Vanilla-javascript-tutorial/spelling/spelling.js
+++ Vanilla-javascript-tutorial/spelling/spelling.js
@@ -3,8 +3,23 @@ words and phrases from a variable
 */
 
 
-let contenido = "primero while SeguNdo TerceRo if cuARto Quinto else if  if function";
-let filtrado = [];
+let contenido = "primero while SeguNdo TerceRo { if cuARto Quinto else if  if function";
+
+var rangeOfNumbersTutorial =
+` function rangeOfNumbers(startNum, endNum) {  
+   </br>  if (startNum === endNum){ 
+   </br>  return [startNum 
+   </br> }else{ 
+   </br> let numeros = rangeOfNumbers(startNum, endNum -1); 
+   </br> numeros.push(endNum); 
+   </br> return numeros; 
+   </br> } 
+   </br> } 
+   
+   </br> console.log(rangeOfNumbers(1, 5)); 
+   </br> console.log(rangeOfNumbers(6, 9)); 
+   </br> console.log(rangeOfNumbers(4, 4));
+   </br> ` ;
 
 
 
@@ -29,12 +44,20 @@ let filtrado = [];
 //  console.log(otro);
 function formateando(suString){
     let produ = "";
-         produ = suString.replace(/if/g, "<span id='if' class='if-class'>if</span>")
-         return produ;
+        produ = suString.replace(/if/g, "<span id='if' class='if-class'>if</span>");
+        produ = produ.replace(/else/g, "<span id='if' class='if-class'>else</span>");
+        produ = produ.replace(/while/g, "<span id='while' class='while-class'>while</span>");
+        produ = produ.replace(/{/g, "<span id='brackets' class='brackets-class'> &#123 </span>");
+        produ = produ.replace(/}/g, "<span id='brackets' class='brackets-class'> &#125 </span>");
+        produ = produ.replace(/function/g, "<span id='functions' class='functions-class'>function</span>");
+        return produ;
 
-}
+    }
+         
+        
+
 
 console.log(formateando(contenido));
 
-document.getElementById("agregar").innerHTML= `<article> ${formateando(contenido)}  </article`;
+document.getElementById("agregar").innerHTML= `<article> ${formateando(rangeOfNumbersTutorial)}  </article`;
 
